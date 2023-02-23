8105a90a9b19414915a5f0a3b618691c7aab03ed
Zmulvihill <zmulvihi@yahoo.com>
2023-02-22 17:09:30 -0700

    added script10 and ran node

---
+++ script10.js
@@ -0,0 +1,8 @@
+//Check For The Presence of an Element With indexOf()
+function quickCheck(arr, elem) {
+    if (arr.indexOf(elem) >= 0) {
+      return true;
+    }
+    return false;
+  }
+  console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
